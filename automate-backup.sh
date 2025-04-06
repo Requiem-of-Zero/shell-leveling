fbc7db8752f89e996e79a24b542dd1a196a782f6
Lelouch <theesamwong@gmail.com>
2025-04-05 20:23:25 -0700

    Adding log file rotations and log directory clearing

--- automate-backup.sh
+++ automate-backup.sh
@@ -8,7 +8,9 @@ TSTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 ARCHIVE_NAME="backup_$SOURCE_BASE-$TSTAMP.tar.gz"
 DESTINATION="$BACKUP_DIR/$ARCHIVE_NAME"
 
-LOG_FILE="$HOME/Library/Logs/automate-backup-error.log"
+MAX_SIZE=50000
+LOGS_DIR="$HOME/Library/Logs/automate-backup-logs/"
+LOG_FILE="$HOME/Library/Logs/automate-backup-logs/automate-backup-error-recent.log"
 
 log_msg(){
   local message="$1"
@@ -16,9 +18,36 @@ log_msg(){
   timestamp="[$(date)]"
 
   echo "$timestamp $message" >> "$LOG_FILE"
-  /usr/bin/logger -t BACKUP_LAUNCHD  "$message"
+  logger -t BACKUP_LAUNCHD  "$message"
 }
 
+rotate_logs(){
+  mkdir -p "$LOGS_DIR"
+  if [[ -f "$LOG_FILE" ]]; then
+    local LOG_SIZE
+    LOG_SIZE=$(wc -c < "$LOG_FILE")
+    if (( LOG_SIZE >= MAX_SIZE  )); then
+      local ROTATED_FILE="$LOGS_DIR/automate-backup-logs-$TSTAMP.log"
+      mv "$LOG_FILE" "$ROTATED_FILE"
+      touch "$LOG_FILE"
+      log_msg "[$(date)] Log roated to $ROTATED_FILE" >> "LOG_FILE"
+
+      local old_logs
+      mapfile -t old_logs < <(ls -1t "$LOGS_DIR"/automate-backup-error-*.log 2>/dev/null)
+      if (( ${#old_logs[@]} > 5 )); then
+        for log in "${old_logs[@]:5}"; do
+          rm -f "$log"
+          log_msg "[$(date)] Deleted old log: $log"
+        done
+      fi
+    fi
+  else
+    log_msg "Log file DNE. Creating log file"
+    touch "$LOG_FILE"
+  fi
+}
+
+rotate_logs
 log_msg "🔁 Launchd triggered automate-backup.sh"
 
 if [[ -d "$BACKUP_DIR" ]]; then
@@ -31,7 +60,7 @@ fi
 
 log_msg "🚀 Starting backup of $SOURCE_DIR"
 
-if tar -cvzf "$DESTINATION" "$SOURCE_DIR"; then
+if tar -czf "$DESTINATION" "$SOURCE_DIR"; then
   log_msg "✅ Backup successful: $DESTINATION"
 else
   log_msg "❌ Backup FAILED at $TSTAMP. Check paths and permissions."
