0a3dcfdad84fbb6e0314c80c9f04fc8969d0132c
Lelouch <theesamwong@gmail.com>
2025-04-05 20:45:39 -0700

    organized files and started shell todo list

---
+++ backup-automation/run-backup-launchd.sh
@@ -0,0 +1,3 @@
+#!/opt/homebrew/bin/bash
+
+"$HOME/Documents/bash-practice/backup-automation/automate-backup.sh" "$HOME/Documents/test" 
--- run-backup-launchd.sh
+++
@@ -1,3 +0,0 @@
-#!/opt/homebrew/bin/bash
-
-"$HOME/Documents/bash-practice/automate-backup.sh" "$HOME/Documents/test" 
